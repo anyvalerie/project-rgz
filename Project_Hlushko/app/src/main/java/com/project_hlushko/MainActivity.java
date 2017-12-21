09ce8a67614e5330e3706094ddf36ff3743264b3
valery_hlushko <valeri131997@gmail.com>
2017-12-21 22:54:19 +0200

    added constants to MainActivity.java

--- Project_Hlushko/app/src/main/java/com/project_hlushko/MainActivity.java
+++ Project_Hlushko/app/src/main/java/com/project_hlushko/MainActivity.java
@@ -3,8 +3,23 @@ package com.project_hlushko;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    TextView tvResult;
+
+    String oper = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +29,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        
+
     }
 }
