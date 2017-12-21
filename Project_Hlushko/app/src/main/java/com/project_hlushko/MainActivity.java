48cdd71ef9ea67cb675ebbd3132b155a10b7d75b
valery_hlushko <valeri131997@gmail.com>
2017-12-21 23:01:40 +0200

    added Menu in MainActivity.java

--- Project_Hlushko/app/src/main/java/com/project_hlushko/MainActivity.java
+++ Project_Hlushko/app/src/main/java/com/project_hlushko/MainActivity.java
@@ -3,12 +3,16 @@ package com.project_hlushko;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
 
     EditText etNum1;
     EditText etNum2;
@@ -72,5 +76,26 @@ public class MainActivity extends Activity implements View.OnClickListener {
             default:
                 break;
         }
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_RESET_ID, 0, "Reset");
+        menu.add(0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET_ID:
+                // очищаем поля
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+                break;
+            case MENU_QUIT_ID:
+                // выход из приложения
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
