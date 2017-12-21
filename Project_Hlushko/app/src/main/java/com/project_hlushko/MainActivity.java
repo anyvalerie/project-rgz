b37fbccb0ec5ababc2d9c77f66fb6a188e147d93
valery_hlushko <valeri131997@gmail.com>
2017-12-21 22:52:53 +0200

    modified MainActivity.java

--- Project_Hlushko/app/src/main/java/com/project_hlushko/MainActivity.java
+++ Project_Hlushko/app/src/main/java/com/project_hlushko/MainActivity.java
@@ -1,13 +1,19 @@
 package com.project_hlushko;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
